c78956bbdad010bdc5e6274fedd1526c0945a9ad
Prasanna Kundu <87819535+pkundu307@users.noreply.github.com>
2023-06-01 22:16:35 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 // here we connect the odm(object document maper) mongoose to the dbms mongodb.
 // mongoose.connect("mongodb+srv://employee-system-user:employee-system-password@employee-system-0.o4qxi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", 
-mongoose.connect("mongodb://127.0.0.1:27017/Narendra-Emp",{
+mongoose.connect("mongodb://127.0.0.1:27017/prasanna",{
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
